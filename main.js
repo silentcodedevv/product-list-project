99cd58f06448c9428b70a41752b32ad9f8ed0a26
Ephraim Djeket <silentcodedaily@gmail.com>
2026-04-10 23:31:04 +0200

    Updates

--- main.js
+++ main.js
@@ -19,28 +19,37 @@ itemContainer.forEach(item => {
     if (incrementBtn) {
       currentAmount++;
       amount.textContent = currentAmount;
+      return currentAmount;
     } else if (decrementBtn) {
       currentAmount--;
       amount.textContent = currentAmount;
+      if (currentAmount === 0) {
+        toggleAddToCart.classList.toggle("hidden");
+        toggleAmountChanger.classList.toggle("hidden");
+        return currentAmount;
+      }
     }
 
-    currentAmount < 1 ? toggleAddToCart.classList.toggle("hidden") && toggleAmountChanger.classList.toggle("hidden") : null;
 
 
     addToCartBtn ? toggleAddToCart.classList.toggle("hidden") && toggleAmountChanger.classList.toggle("hidden") : null;
 
     if (!addToCartBtn) return;
 
-    yourCart.insertAdjacentHTML("afterbegin", `
-    <li class="flex justify-between border-b border-smalltext pb-4">
-            <div class="item-info-container flex gap-8"> <img class="w-16 rounded-sm border-[#6AB3FE] border-2"
-                src=${item.querySelector("img").src} alt=${item.querySelector("img").alt}>
-              <div class="item-text-container flex flex-col my-auto gap-2">
-                <h3 class="text-xl font-bold">${nameOfProduct}</h3>
-                <small class="text-[16px] font-extralight text-[#8B9FFF]">${currentAmount} x $${item.querySelector("span").textContent}</small>
-              </div>
-            </div>
-            <img class="w-4 h-4 cursor-pointer" src="images/ion--trash 1.png" alt="icon trash">
-          </li>`);
+    if (item) {
+      yourCart.insertAdjacentHTML("afterbegin", `
+      <li class="flex justify-between border-b border-smalltext pb-4">
+              <div class="item-info-container flex gap-8"> <img class="w-16 rounded-sm border-[#6AB3FE] border-2"
+                  src=${item.querySelector("img").src} alt=${item.querySelector("img").alt}>
+                <div class="item-text-container flex flex-col my-auto gap-2">
+                  <h3 class="text-xl font-bold">${nameOfProduct}</h3>
+                  <small class="text-[16px] font-extralight text-[#8B9FFF]">${currentAmount} x $${item.querySelector("span").textContent * currentAmount}</small >
+                </div >
+              </div >
+      <img class="w-4 h-4 cursor-pointer" src="images/ion--trash 1.png" alt="icon trash">
+      </li>`);
+    } else if (!yourCart.querySelector(".item")) {
+      yourCart.insertAdjacentElement("afterbegin", "Your added items will appear here");
+    }
   })
 })
