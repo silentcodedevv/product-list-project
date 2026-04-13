ba93eba2fe13dfcab4b91e593c4f3fec038ff6da
Ephraim Djeket <silentcodedaily@gmail.com>
2026-04-13 02:19:19 +0200

    Added cart object

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const itemContainer = document.querySelectorAll(".item-container");
 const yourCart = document.getElementById("your-cart");
 
+
 itemContainer.forEach(item => {
   item.addEventListener("click", (e) => {
     const addToCartBtn = e.target.classList.contains("add-to-cart-btn");
@@ -8,22 +9,49 @@ itemContainer.forEach(item => {
     const toggleAddToCart = item.querySelector(".add-to-cart-btn")
     const toggleAmountChanger = item.querySelector(".amount-changer");
     const nameOfProduct = item.querySelector("h2").textContent;
-    let amount = toggleAmountChanger.querySelector(".amount");
+    let currentAmount = item.querySelector(".amount");
     const incrementBtn = e.target.classList.contains("increment");
     const decrementBtn = e.target.classList.contains("decrement");
 
-    let currentAmount = Number(amount.textContent);
-    currentAmount === 0 ? currentAmount++ : null;
-    amount.textContent = currentAmount;
+    const cart = {
+      name: nameOfProduct,
+      amount: 1,
+      price: Number(item.querySelector(".price").textContent)
+    }
+
+    currentAmount.textContent = cart.amount;
+
+    function renderCart() {
+      yourCart.innerHTML = "";
+
+
+      yourCart.insertAdjacentHTML("afterbegin", `
+      <li class="flex mt-2 justify-between border-b border-smalltext pb-4">
+              <div class="item-info-container flex gap-8"> <img class="w-16 rounded-sm border-[#6AB3FE] border-2"
+                  src=${item.querySelector("img").src} alt=${item.querySelector("img").alt}>
+                <div class="item-text-container flex flex-col my-auto gap-2">
+                  <h2 class="text-xl font-bold">${cart.name}</h2>
+                  <small class="text-[16px] font-extralight text-[#8B9FFF]">${cart.amount} x $${cart.price * cart.amount}</small >
+                </div >
+              </div >
+      <img class="w-4 h-4 cursor-pointer" src="images/ion--trash 1.png" alt="icon trash">
+      </li>`);
+    }
 
     if (incrementBtn) {
-      currentAmount++;
-      amount.textContent = currentAmount;
-      return currentAmount;
+      cart.amount++;
+      currentAmount.textContent = cart.amount;
+      renderCart();
+      if (!cart.name) {
+        renderCart();
+      } else {
+        cart.amount++;
+      }
     } else if (decrementBtn) {
-      currentAmount--;
-      amount.textContent = currentAmount;
-      if (currentAmount === 0) {
+      cart.amount--;
+      currentAmount.textContent = cart.amount;
+      renderCart();
+      if (cart.amount === 0) {
         toggleAddToCart.classList.toggle("hidden");
         toggleAmountChanger.classList.toggle("hidden");
 
@@ -45,19 +73,5 @@ itemContainer.forEach(item => {
     addToCartBtn ? toggleAddToCart.classList.toggle("hidden") && toggleAmountChanger.classList.toggle("hidden") : null;
 
     if (!addToCartBtn) return;
-
-    if (item) {
-      yourCart.insertAdjacentHTML("afterbegin", `
-      <li class="flex mt-2 justify-between border-b border-smalltext pb-4">
-              <div class="item-info-container flex gap-8"> <img class="w-16 rounded-sm border-[#6AB3FE] border-2"
-                  src=${item.querySelector("img").src} alt=${item.querySelector("img").alt}>
-                <div class="item-text-container flex flex-col my-auto gap-2">
-                  <h2 class="text-xl font-bold">${nameOfProduct}</h2>
-                  <small class="text-[16px] font-extralight text-[#8B9FFF]">${currentAmount} x $${item.querySelector("span").textContent * currentAmount}</small >
-                </div >
-              </div >
-      <img class="w-4 h-4 cursor-pointer" src="images/ion--trash 1.png" alt="icon trash">
-      </li>`);
-    }
   })
 })
